9f75239f9ff26fa3b3fe087dc34badaeb36141bc
Ross Lawley <ross.lawley@gmail.com>
2017-01-26 17:22:18 +0000

    Updated publish

--- .evergreen/publish.sh
+++ .evergreen/publish.sh
@@ -10,14 +10,15 @@ set -o errexit  # Exit the script with error if any of the commands fail
 
 JAVA_HOME="/opt/java/jdk8"
 PUBLISH_PROPERTIES_FILE=${PROJECT_DIRECTORY}/.publishProperties
+SECRING_FILE=${PROJECT_DIRECTORY}/secring.gpg
 
-echo ${RING_FILE_GPG_BASE64} | base64 -d > ${PROJECT_DIRECTORY}/secring.gpg
+echo ${RING_FILE_GPG_BASE64} | base64 -d > ${SECRING_FILE}
 
 echo nexusUsername=${NEXUS_USERNAME} > $PUBLISH_PROPERTIES_FILE
 echo nexusPassword=${NEXUS_PASSWORD} >> $PUBLISH_PROPERTIES_FILE
 echo signing.keyId=${SIGNING_KEY_ID} >> $PUBLISH_PROPERTIES_FILE
 echo signing.password=${SIGNING_PASSWORD} >> $PUBLISH_PROPERTIES_FILE
-echo signing.secretKeyRingFile=${PROJECT_DIRECTORY}/secring.gpg >> $PUBLISH_PROPERTIES_FILE
+echo signing.secretKeyRingFile=${SECRING_FILE} >> $PUBLISH_PROPERTIES_FILE
 
 echo "Publishing snapshots"
 
