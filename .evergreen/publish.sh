cbc43b8c7bd8565e3ae34aa93410f2297c610634
Ross Lawley <ross.lawley@gmail.com>
2017-01-27 10:18:10 +0000

    added coverage

--- .evergreen/publish.sh
+++ .evergreen/publish.sh
@@ -12,6 +12,8 @@ JAVA_HOME="/opt/java/jdk8"
 PUBLISH_PROPERTIES_FILE=${PROJECT_DIRECTORY}/.publishProperties
 SECRING_FILE=${PROJECT_DIRECTORY}/secring.gpg
 
+trap "rm ${PUBLISH_PROPERTIES_FILE}; rm ${SECRING_FILE}; exit" EXIT HUP
+
 echo ${RING_FILE_GPG_BASE64} | base64 -d > ${SECRING_FILE}
 
 echo nexusUsername=${NEXUS_USERNAME} > $PUBLISH_PROPERTIES_FILE
@@ -21,7 +23,6 @@ echo signing.password=${SIGNING_PASSWORD} >> $PUBLISH_PROPERTIES_FILE
 echo signing.secretKeyRingFile=${SECRING_FILE} >> $PUBLISH_PROPERTIES_FILE
 
 echo "Publishing snapshots"
-
 
 ./sbt -java-home $JAVA_HOME version
 ./sbt -java-home $JAVA_HOME +clean +publishSnapshot -DpublishProperties=$PUBLISH_PROPERTIES_FILE
